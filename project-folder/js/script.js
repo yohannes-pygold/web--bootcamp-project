220a09f4747012acb548a417227ec4a65f172de6
yohannes-pygold <yohannesworku1997@gmail.com>
2026-05-05 19:46:15 +0300

    Update

---
+++ project-folder/js/script.js
@@ -0,0 +1,8 @@
+/*I am Yohannes Worku and I am sorry for the late submission.
+I have been taking mid exams and I can't get the time to complete it in time.
+And also I practiced html and css beyond what you and the club gave us.
+But, Due to the reason I mentioned, I can't get the time to practice 
+how I can use JavaScript into my page.So, This is the whole structure 
+and presentation I recreated(not interactive as JavaScript is not used.) 
+though I used some advanced css features like transitions.
+I will practice JavaScript to use it in the coming projects*/
